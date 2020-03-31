403848529f4a432cef67d2c68b9c398877c80c7d
lilly <ellis.l@rangiruru.school.nz>
2020-03-31 21:35:48 +1300

    Box is lockable and unlockable.

--- src/index.svelte
+++ src/index.svelte
@@ -1,3 +1,27 @@
-<h1>Svelte</h1>
+<script>
+ let box = {
+     locked: true, 
+     contents: [
+         "Thing 1",
+         "Thing 2",
+         "Things 3"
+     ]
+ }
 
-<p>Welcome to coding with Svelte!</p>
+ function lock(){
+     box.locked = true
+ }
+ function unlock(){
+     box.locked = false
+ }
+</script>
+
+<h1>the box </h1>
+
+{#if box.locked}
+<p>The box is locked.</p>
+<button on:click={unlock}>Unlock</button>
+{:else}
+<p>The box is unlocked </p>
+<button on:click={lock}>Lock</button>
+{/if}
